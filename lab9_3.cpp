bfc9330db0e52e2e54d4c896b6734d5588d849f0
r1chwo0n <woonzy09@gmail.com>
2022-12-23 16:05:10 +0700

    lab9_3.cpp completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,32 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+  int age, height;
+  signed long long int bounty;
+  cout << "Enter your age: ";
+  cin >> age;
+  if (age <= 25){
+    cout << "Enter your height: ";
+    cin >> height;
+      if (height < 100) cout << "Your character = Chopper";
+      else if (height < 180) cout << "Your character = Usopp";
+      else 
+        {cout << "Enter your bounty: ";
+        cin >> bounty;
+          if(bounty > 1.1e9) cout << "Your character = Zoro"; 
+          else cout << "Your character = Sanji"; }
+    
+  }else if (age <= 60){
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+    if (bounty > 500e6) cout << "Your character = Jinbe"; 
+    else cout << "Your character = Franky"; 
+  }else cout << "Your character = Brook";
+  
+}
